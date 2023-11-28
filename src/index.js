378f0db15f0fd6d681e5cf616bbc51fb08fa56d6
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-11-28 19:23:09 +0700

    add new route

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import express from 'express'
+import express from "express"
+import { publicRouter } from "./routes/public-api.js"
+import { roleRouter } from "./routes/api.js"
+import { errorMiddleware } from "./middleware/error-middleware.js"
 
-const router = new express.Router()
+export const app = express()
 
+app.use(express.json())
 app.use(errorMiddleware)
+app.use(publicRouter)
+app.use(roleRouter)
+
+app.listen(process.env.PORT, () => {
+    console.log('listening on port 3000')
+    // logger.info("App start");
+})
