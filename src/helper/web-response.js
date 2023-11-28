0c9c2d626997f733b36fc391100a823460882de0
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-11-28 20:54:41 +0700

    add status to structure

--- src/helper/web-response.js
+++ src/helper/web-response.js
@@ -1,6 +1,7 @@
-const WebResponse = (code, msg, data) => {
+const WebResponse = (code, status, msg, data) => {
     return {
         code: code,
+        status: status,
         msg: msg,
         data: data
     }
