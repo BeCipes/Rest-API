ebbd807d2f420e6df68eefe8250863f0cd5b8215
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-11-28 19:23:20 +0700

    update authmiddleware

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -1,24 +1,40 @@
-import { prismaClient } from "../application/database.js";
+import { prismaClient } from "../application/database.js"
 
 export const authMiddleware = async (req, res, next) => {
-    const token = req.get('Authorization');
+    const token = req.get('Authorization')
     if (!token) {
         res.status(401).json({
             errors: "Unauthorized"
-        }).end();
-    } else {
-        const user = await prismaClient.user.findFirst({
-            where: {
-                token: token
-            }
-        });
-        if (!user) {
-            res.status(401).json({
-                errors: "Unauthorized"
-            }).end();
-        } else {
-            req.user = user;
-            next();
-        }
+        }).end()
     }
+    
+    const user = await prismaClient.user.findFirst({
+        where: {
+            token: token
+        },
+        include: {
+            role: {
+                select: {
+                    role_name: true
+                }
+            }
+        }
+    })
+    
+    if (!user) {
+        res.status(401).json({
+            errors: "Unauthorized"
+        }).end()
+    }
+
+    req.user = user
+    const userRole = user.role?.role_name.toLowerCase()
+
+    if (userRole !== 'user') {
+        return res.status(403).json({
+            errors: "Forbidden"
+        }).end()
+    }
+
+    next()
 }
