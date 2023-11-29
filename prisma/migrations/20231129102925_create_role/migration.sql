5c2638d3011931910b855276db1f29562331eeaf
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-11-29 17:30:52 +0700

    make migration for user & role

---
+++ prisma/migrations/20231129102925_create_role/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `role` (
+    `id_role` INTEGER NOT NULL AUTO_INCREMENT,
+    `role_name` VARCHAR(100) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `role_role_name_key`(`role_name`),
+    PRIMARY KEY (`id_role`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `users` ADD CONSTRAINT `users_id_role_fkey` FOREIGN KEY (`id_role`) REFERENCES `role`(`id_role`) ON DELETE RESTRICT ON UPDATE CASCADE;
