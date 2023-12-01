c1e98cc0b58e6b6c93d0760728eaeda5cddaed8b
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-12-01 17:05:50 +0700

    add new routes to kategori resep

--- src/application/web.js
+++ src/application/web.js
@@ -2,6 +2,7 @@ import express from "express"
 import { publicRouter } from "./../routes/public-api.js"
 import { roleRouter } from "./../routes/role-api.js"
 import { userRouter } from "./../routes/user-api.js"
+import { kategoriResepRouter } from "../routes/kategori_resep-api.js"
 import { errorMiddleware } from "./../middleware/error-middleware.js"
 
 const app = express()
@@ -10,9 +11,13 @@ app.use(express.json())
 
 const apiPrefix = "/api"
 
+// Public routes API (user & admin)
 app.use(apiPrefix, publicRouter)
-app.use(apiPrefix, roleRouter)
+
+// Admin routes API (admin only)
 app.use(apiPrefix, userRouter)
+app.use(apiPrefix, roleRouter)
+app.use(apiPrefix, kategoriResepRouter)
 
 app.use(errorMiddleware)
 
