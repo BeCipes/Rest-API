bfa063fb67737d32b8d3257e0e271e0ea8e31c7a
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-12-02 01:56:08 +0700

    add dotenv package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { app } from "./application/web.js"
 import { logger } from "./application/logging.js"
+import dotenv from "dotenv"
+
+dotenv.config({ path: './config/.env' })
 
 app.listen(process.env.PORT, () => {
     // console.log('listening on port 3000')
