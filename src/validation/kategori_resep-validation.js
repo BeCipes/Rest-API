9a501752138bcc7afd91e100c93726979afd3bbd
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-12-03 23:24:40 +0700

    done validation for kategori resep

---
+++ src/validation/kategori_resep-validation.js
@@ -0,0 +1,20 @@
+import Joi from "joi"
+
+const createKategoriResepValidation = Joi.object({
+    id_kategori: Joi.number().min(1).positive().required(),
+    id_resep: Joi.number().min(1).positive().required()
+})
+
+const updateKategoriResepValidation = Joi.object({
+    id: Joi.number().min(1).positive().required(),
+    id_kategori: Joi.number().min(1).positive().required(),
+    id_resep: Joi.number().min(1).positive().required()
+})
+
+const getKategoriResepValidation = Joi.number().min(1).positive().required()
+
+export {
+    createKategoriResepValidation,
+    updateKategoriResepValidation,
+    getKategoriResepValidation
+}
