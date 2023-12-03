2d0fadfbf8d6ed551f76e046ad3edbc424ac317d
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-12-03 23:24:16 +0700

    done validation for artikel

--- src/validation/artikel-validation.js
+++ src/validation/artikel-validation.js
@@ -2,22 +2,21 @@ import Joi from "joi"
 
 const createArtikelValidation = Joi.object({
     headline: Joi.string().max(100).required(),
-    image: Joi.string().max(100).required(),
-    content: Joi.string().max(100).required(),
-    writer: Joi.string().max(100).required(),
-    source: Joi.string().max(100).required(),
-    category: Joi.string().max(100).required(),
-    created_by: Joi.string().max(100).required(),
+    gambar: Joi.string().max(100).required(),
+    isi: Joi.string().max(100).required(),
+    penulis: Joi.string().max(100).required(),
+    sumber: Joi.string().max(100).required(),
+    id_kategori: Joi.number().min(1).positive().required(),
 })
 
 const updateArtikelValidation = Joi.object({
-    id_artikel: Joi.number().min(1).positive().required(),
+    id: Joi.number().min(1).positive().required(),
     headline: Joi.string().max(100).required(),
-    image: Joi.string().max(100).required(),
-    content: Joi.string().max(100).required(),
-    writer: Joi.string().max(100).required(),
-    source: Joi.string().max(100).required(),
-    category: Joi.string().max(100).required()
+    gambar: Joi.string().max(100).required(),
+    isi: Joi.string().max(100).required(),
+    penulis: Joi.string().max(100).required(),
+    sumber: Joi.string().max(100).required(),
+    id_kategori: Joi.number().min(1).positive().required(),
 })
 
 const getArtikelValidation = Joi.number().min(1).positive().required()
