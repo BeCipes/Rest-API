0ec262c2c6a5dc7234f083d8a2f5cd84aaaef051
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-12-04 02:55:55 +0700

    refactor + add new page not found middleware

--- src/app/web.js
+++ src/app/web.js
@@ -4,15 +4,14 @@ import { roleRouter } from "../routes/role-api.js"
 import { userRouter } from "../routes/user-api.js"
 import { kategoriResepRouter } from "../routes/kategori_resep-api.js"
 import { errorMiddleware } from "../middleware/error-middleware.js"
-import { configureMailer } from "../helper/mailer.js"
+import { notfoundMiddleware } from "../middleware/not-found-middleware.js"
 import cors from "cors"
 
 const app = express()
 
-configureMailer(app)
-
 app.use(cors())
 app.use(express.json())
+
 
 const apiPrefix = "/api"
 
@@ -25,5 +24,6 @@ app.use(apiPrefix, roleRouter)
 app.use(apiPrefix, kategoriResepRouter)
 
 app.use(errorMiddleware)
+app.use(notfoundMiddleware)
 
 export { app }
