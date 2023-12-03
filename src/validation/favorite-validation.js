feb424dd39501c1374b3a4d6f4c148a6ad70277e
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-12-03 23:24:27 +0700

    done validation for favorite

---
+++ src/validation/favorite-validation.js
@@ -0,0 +1,20 @@
+import Joi from "joi"
+
+const createFavoriteValidation = Joi.object({
+    id_user: Joi.number().min(1).positive().required(),
+    id_resep: Joi.number().min(1).positive().required(),
+})
+
+const updateFavoriteValidation = Joi.object({
+    id: Joi.number().min(1).positive().required(),
+    id_user: Joi.number().min(1).positive().required(),
+    id_resep: Joi.number().min(1).positive().required(),
+})
+
+const getFavoriteValidation = Joi.number().min(1).positive().required()
+
+export {
+    createFavoriteValidation,
+    updateFavoriteValidation,
+    getFavoriteValidation
+}
