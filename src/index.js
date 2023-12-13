51d32a65a7f3166679a0b465e7aec07b4ad0a5d7
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-12-13 21:53:02 +0700

    refactor

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import { logger } from "./app/logging.js"
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    // console.log('listening on port 3000')
     logger.info(`App start on port ${process.env.PORT}`)
 })
