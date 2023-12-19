3a1183c0019e2fcc3a195a8f416cf82a7c5ce17e
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-12-19 10:04:36 +0700

    update datatype

--- src/validation/resep-validation.js
+++ src/validation/resep-validation.js
@@ -4,8 +4,8 @@ const createResepValidation = Joi.object({
     nama_resep: Joi.string().max(100).required(),
     deskripsi: Joi.string().max(100).required(),
     gambar: Joi.string().max(100).required(),
-    bahan: Joi.string().max(100).required(),
-    informasi_gizi: Joi.string().max(100).required(),
+    bahan: Joi.array().max(100).required(),
+    informasi_gizi: Joi.object().max(100).required(),
     createdBy: Joi.string().min(36).max(36).required(),
 })
 
@@ -14,8 +14,8 @@ const updateResepValidation = Joi.object({
     nama_resep: Joi.string().max(100).required(),
     deskripsi: Joi.string().max(100).required(),
     gambar: Joi.string().max(100).required(),
-    bahan: Joi.string().max(100).required(),
-    informasi_gizi: Joi.string().max(100).required(),
+    bahan: Joi.array().max(100).required(),
+    informasi_gizi: Joi.object().max(100).required()
 })
 
 const getResepValidation = Joi.number().min(1).positive().required()
