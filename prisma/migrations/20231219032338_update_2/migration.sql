dd8d347df9eb0df20b49c18f3cdde12e1ece2148
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-12-19 10:23:47 +0700

    update datatype

---
+++ prisma/migrations/20231219032338_update_2/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `artikel` MODIFY `isi` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `bahan` MODIFY `deskripsi` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `teknik` MODIFY `description` TEXT NOT NULL;
