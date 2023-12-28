50fdef081f794910a9872c6fc76eebbe1a737c7f
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-12-28 09:41:41 +0700

    update

--- src/middleware/public-middleware.js
+++ src/middleware/public-middleware.js
@@ -127,29 +127,12 @@ const uploadMiddleware = async (req, res, next) => {
         const user = await prismaClient.user.findFirst({
             where: {
                 id: decodedToken.id,
-            },
-            include: {
-                role: {
-                    select: {
-                        role_name: true
-                    }
-                }
             }
         })
 
         if (!user) {
             const response = ErrorWebResponse(401, "Unauthorized")
             res.status(401).json(response).end()
-            
-            return
-        }
-
-        req.user = user
-        const userRole = user.role?.role_name.toLowerCase()
-
-        if (userRole !== 'user' || userRole !== 'admin') {
-            const response = ErrorWebResponse(403, "Forbidden")
-            res.status(403).json(response).end()
             
             return
         }
