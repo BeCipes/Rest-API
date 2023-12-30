9fc27301b5eae996ee6d1453da10546a2fe6ee0e
Sytrwn <alfin.syatriawan64@gmail.com>
2023-12-30 14:39:29 +0700

    Ganti kata austin

--- config/cors-config.js
+++ config/cors-config.js
@@ -1,9 +1,9 @@
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:' + PORT,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-}
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
-export { corsOptions }
+export { corsOptions };
