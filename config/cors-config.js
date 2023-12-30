8b93f0ed25bed3102b54d1d0dd1e7c455c97b8a8
AustinNick <46616395+AustinNick@users.noreply.github.com>
2023-12-30 19:55:46 +0700

    update cors policy

--- config/cors-config.js
+++ config/cors-config.js
@@ -1,7 +1,13 @@
 const PORT = process.env.PORT || 3000
+const PORT_ADIT = 5173
 
 const corsOptions = {
-    origin: 'https://backend-development-becipes.fly.dev/',
+    // origin: function (origin, callback) {
+    //     const allowedOrigins = ['http://localhost:' + PORT, 'http://localhost:' + PORT_ADIT]
+    //     const isAllowed = allowedOrigins.includes(origin)
+    //     callback(null, isAllowed)
+    // },
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
 }
